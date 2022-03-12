043562ab2a73369a0405060cee9e5d7be169f079
jxch <15553598935@163.com>
2022-03-12 13:23:42 +0800

    安装jdk1.8 & 启动jenkins

--- jenkins_install_yum.sh
+++ jenkins_install_yum.sh
@@ -4,4 +4,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 yum update -y 
+yum install -y java-1.8.0-openjdk*
 yum install -y jenkins
+
+java -jar /usr/lib/jenkins/jenkins.war --httpPort=9091 > /var/log/jenkins/jenkins-$(date +%Y-%m-%d).log 2>&1 &
