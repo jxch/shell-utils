b4c3b38cea7ade4f5bc889e2976722acd6076a99
jxch <15553598935@163.com>
2022-03-12 13:30:08 +0800

    取消 jenkins 启动

--- jenkins_install_yum.sh
+++ jenkins_install_yum.sh
@@ -6,5 +6,3 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum update -y 
 yum install -y java-1.8.0-openjdk*
 yum install -y jenkins
-
-java -jar /usr/lib/jenkins/jenkins.war --httpPort=9091 > /var/log/jenkins/jenkins-$(date +%Y-%m-%d).log 2>&1 &
