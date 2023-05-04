7cf70235969e27d7771d85af9350b8b23beb0848
jxch <qianbuhan@proton.me>
2023-05-04 18:32:53 +0800

    winget

---
+++ winget-install.ps1
@@ -0,0 +1,46 @@
+wsl --install
+
+winget install 7zip.7zip -l D:\Programs\7zip --accept-package-agreements
+winget install Google.Chrome -l D:\Programs\Google\Chrome --accept-package-agreements
+winget install Mozilla.Firefox -l D:\Programs\Mozilla\Firefox --accept-package-agreements
+winget install Docker.DockerDesktop -l D:\Programs\Docker\DockerDesktop --accept-package-agreements
+winget install Tencent.WeChat -l D:\Programs\Tencent\WeChat --accept-package-agreements
+winget install Tencent.TIM -l D:\Programs\Tencent\TIM --accept-package-agreements
+winget install Daum.PotPlayer -l D:\Programs\Daum\PotPlayer --accept-package-agreements
+winget install Ditto.Ditto -l D:\Programs\Ditto --accept-package-agreements
+winget install UderzoSoftware.SpaceSniffer -l D:\Programs\SpaceSniffer --accept-package-agreements
+winget install Microsoft.VisualStudioCode -l D:\Programs\Microsoft\VisualStudioCode --accept-package-agreements
+winget install SublimeHQ.SublimeText.4 -l D:\Programs\SublimeText --accept-package-agreements
+winget install Git.Git -l D:\Programs\Git --accept-package-agreements
+winget install JetBrains.Toolbox -l D:\Programs\JetBrains\Toolbox --accept-package-agreements
+winget install Sinew.Enpass -l D:\Programs\Enpass --accept-package-agreements
+winget install OpenJS.NodeJS.LTS -l D:\Programs\NodeJS\LTS --accept-package-agreements
+winget install voidtools.Everything -l D:\Programs\Everything --accept-package-agreements
+winget install File-New-Project.EarTrumpet -l D:\Programs\EarTrumpet --accept-package-agreements
+
+
+
+# 以下无法安装（连接不到服务器）
+winget install Initex.Proxifier -l D:\Programs\Proxifier --accept-package-agreements
+winget install calibre.calibre -l D:\Programs\Calibre --accept-package-agreements
+winget install Notepad++.Notepad++ -l D:\Programs\Notepad --accept-package-agreements
+winget install QuestSoft.QTranslate -l D:\Programs\QTranslate --accept-package-agreements
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
