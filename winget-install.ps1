236faa228ce929434025af48dfda6be2fcbfb05f
jxch <qianbuhan@proton.me>
2023-05-10 22:27:56 +0800

    cloudpan189-go

--- winget-install.ps1
+++ winget-install.ps1
@@ -25,6 +25,7 @@ winget install VMware.WorkstationPro -l D:\Programs\VMware\WorkstationPro --acce
 winget install Xmind.Xmind -l D:\Programs\Xmind --accept-package-agreements
 winget install Thunder.Thunder -l D:\Programs\Thunder --accept-package-agreements
 winget install Postman.Postman -l D:\Programs\Postman --accept-package-agreements
+winget install tickstep.cloudpan189-go -l D:\Programs\cloudpan189 --accept-package-agreements
 
 # 以下软件安装需要代理翻墙
 proxychains winget install calibre.calibre -l D:\Programs\Calibre --accept-package-agreements
