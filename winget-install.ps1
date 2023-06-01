ee9aab5e27946e9fb8434ca1b8e79adf4a8f0e07
jxch <qianbuhan@proton.me>
2023-06-02 01:45:53 +0800

    注释：软件相关的目录

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,34 +1,53 @@
 wsl --install
 
 winget install 7zip.7zip -l D:\Programs\7zip --accept-package-agreements
+# C:\Program Files\Google\Chrome
+# C:\Program Files (x86)\Google
 winget install Google.Chrome -l D:\Programs\Google\Chrome --accept-package-agreements
+# C:\Program Files\Mozilla Firefox
 winget install Mozilla.Firefox -l D:\Programs\Mozilla\Firefox --accept-package-agreements
+# C:\Program Files\Docker
+# C:\ProgramData\DockerDesktop
 winget install Docker.DockerDesktop -l D:\Programs\Docker\DockerDesktop --accept-package-agreements
+# C:\Program Files (x86)\Tencent\WeChat
+# C:\ProgramData\Tencent
 winget install Tencent.WeChat -l D:\Programs\Tencent\WeChat --accept-package-agreements
 winget install Tencent.TIM -l D:\Programs\Tencent\TIM --accept-package-agreements
 winget install Daum.PotPlayer -l D:\Programs\Daum\PotPlayer --accept-package-agreements
 winget install Ditto.Ditto -l D:\Programs\Ditto --accept-package-agreements
 winget install UderzoSoftware.SpaceSniffer -l D:\Programs\SpaceSniffer --accept-package-agreements
 winget install Microsoft.VisualStudioCode -l D:\Programs\Microsoft\VisualStudioCode --accept-package-agreements
+winget install Microsoft.VisualStudioCode.Insiders -l D:\Programs\Microsoft\VisualStudioCodeInsiders --accept-package-agreements
 winget install SublimeHQ.SublimeText.4 -l D:\Programs\SublimeText --accept-package-agreements
 winget install Git.Git -l D:\Programs\Git --accept-package-agreements
 winget install JetBrains.Toolbox -l D:\Programs\JetBrains\Toolbox --accept-package-agreements
+# C:\Program Files (x86)\Enpass
 winget install Sinew.Enpass -l D:\Programs\Enpass --accept-package-agreements
+# C:\Program Files\nodejs
 winget install OpenJS.NodeJS.LTS -l D:\Programs\NodeJS\LTS --accept-package-agreements
+# C:\Program Files\Everything
 winget install voidtools.Everything -l D:\Programs\Everything --accept-package-agreements
 winget install File-New-Project.EarTrumpet -l D:\Programs\EarTrumpet --accept-package-agreements
+# C:\Program Files\Adobe 
+# C:\Program Files (x86)\Adobe
+# C:\ProgramData\Adobe
 winget install XPDLPKWG9SW2WD -l D:\Programs\Adobe\Cloud --accept-package-agreements
 winget install Anaconda.Anaconda3 -l D:\Programs\Anaconda3 --accept-package-agreements
 winget install Tencent.EDU -l D:\Programs\Tencent\EDU --accept-package-agreements
 winget install Alibaba.Yuque -l D:\Programs\Alibaba\Yuque --accept-package-agreements
+# C:\Program Files (x86)\VMware
+# C:\ProgramData\VMware
 winget install VMware.WorkstationPro -l D:\Programs\VMware\WorkstationPro --accept-package-agreements
 winget install Xmind.Xmind -l D:\Programs\Xmind --accept-package-agreements
 winget install Thunder.Thunder -l D:\Programs\Thunder --accept-package-agreements
 winget install Postman.Postman -l D:\Programs\Postman --accept-package-agreements
 winget install tickstep.cloudpan189-go -l D:\Programs\cloudpan189 --accept-package-agreements
+# C:\Program Files\Cloudflare
+# C:\ProgramData\Cloudflare
 winget install Cloudflare.Warp -l D:\Programs\Cloudflare\Warp --accept-package-agreements
 winget install RubyInstallerTeam.RubyWithDevKit.3.2 -l D:\Programs\Ruby --accept-package-agreements
 winget install TorProject.TorBrowser -l D:\Programs\TorBrowser --accept-package-agreements
+# C:\Program Files\Rainmeter
 winget install Rainmeter.Rainmeter -l D:\Programs\Rainmeter --accept-package-agreements
 winget install GnuCash.GnuCash -l D:\Programs\GnuCash --accept-package-agreements
 winget install Gyan.FFmpeg -l D:\Programs\Gyan\FFmpeg --accept-package-agreements
@@ -40,6 +59,7 @@ winget install AnyTXT.AnyTXTSearcher -l D:\Programs\AnyTXT --accept-package-agre
 winget install QL-Win.QuickLook -l D:\Programs\QuickLook --accept-package-agreements
 
 # 以下软件安装需要代理翻墙
+# C:\Program Files\Calibre2
 proxychains winget install calibre.calibre -l D:\Programs\Calibre --accept-package-agreements
 proxychains winget install Notepad++.Notepad++ -l D:\Programs\Notepad --accept-package-agreements
 proxychains winget install QuestSoft.QTranslate -l D:\Programs\QTranslate --accept-package-agreements
