8c5341930971a74898c7e2811fe905982cffd8f3
jxch <qianbuhan@proton.me>
2024-03-07 07:29:47 +0800

    winget

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,58 +1,75 @@
+wsl --update
+winget source remove winget
+winget source add winget https://mirrors.ustc.edu.cn/winget-source
+winget install Docker.DockerDesktop 
 wsl --install -d Ubuntu
-winget install Docker.DockerDesktop --accept-package-agreements
 
-winget install 7zip.7zip --accept-package-agreements
-winget install Microsoft.VisualStudioCode --accept-package-agreements
-winget install Google.Chrome --accept-package-agreements
-winget install Mozilla.Firefox --accept-package-agreements
-winget install Tencent.WeChat --accept-package-agreements
-winget install Tencent.TIM --accept-package-agreements
-winget install Daum.PotPlayer --accept-package-agreements
-winget install VideoLAN.VLC --accept-package-agreements
-winget install Sinew.Enpass --accept-package-agreements
-winget install voidtools.Everything --accept-package-agreements
-winget install JetBrains.Toolbox --accept-package-agreements
-winget install File-New-Project.EarTrumpet --accept-package-agreements
-winget install Anaconda.Anaconda3 --accept-package-agreements
-winget install Axosoft.GitKraken --accept-package-agreements
-winget install SublimeHQ.SublimeText.4 --accept-package-agreements
-winget install VMware.WorkstationPro --accept-package-agreements
-winget install Xmind.Xmind --accept-package-agreements
-winget install Thunder.Thunder --accept-package-agreements
-winget install Postman.Postman --accept-package-agreements
-winget install ScooterSoftware.BeyondCompare4 --accept-package-agreements
-winget install AnyTXT.AnyTXTSearcher --accept-package-agreements
-winget install DigitalScholar.Zotero --accept-package-agreements
-winget install Microsoft.Skype --accept-package-agreements
-winget install tickstep.cloudpan189-go --accept-package-agreements
-winget install TrackerSoftware.PDF-XChangeEditor --accept-package-agreements
-winget install Git.Git --accept-package-agreements
-winget install Ditto.Ditto --accept-package-agreements
-winget install Nvidia.CUDA --version 11.3 --accept-package-agreements
-winget install CoreyButler.NVMforWindows --accept-package-agreements
-winget install Microsoft.VisualStudio.2022.BuildTools --accept-package-agreements
-winget install Microsoft.VisualStudio.2022.Community --accept-package-agreements
+winget install 2dust.v2rayN 
+winget install 7zip.7zip 
+winget install Microsoft.VisualStudioCode 
+winget install Google.Chrome 
+winget install Mozilla.Firefox 
+winget install Tencent.WeChat 
+winget install Tencent.TIM 
+winget install Daum.PotPlayer 
+winget install VideoLAN.VLC 
+winget install Sinew.Enpass 
+winget install voidtools.Everything 
+winget install LocalSend.LocalSend
+winget install JetBrains.Toolbox 
+winget install File-New-Project.EarTrumpet 
+winget install Anaconda.Anaconda3 
+winget install Axosoft.GitKraken 
+winget install SublimeHQ.SublimeText.4 
+winget install VMware.WorkstationPro 
+winget install Xmind.Xmind 
+winget install Thunder.Thunder 
+winget install Postman.Postman 
+winget install ScooterSoftware.BeyondCompare4 
+winget install AnyTXT.AnyTXTSearcher 
+winget install DigitalScholar.Zotero 
+winget install Microsoft.Skype 
+winget install tickstep.cloudpan189-go 
+winget install TrackerSoftware.PDF-XChangeEditor 
+winget install Git.Git 
+winget install GitHub.GitHubDesktop
+winget install Ditto.Ditto 
+winget install NGWIN.PicPick 
+winget install Nvidia.CUDA --version 11.3 
+winget install CoreyButler.NVMforWindows 
+winget install Microsoft.VisualStudio.2022.BuildTools 
+winget install Microsoft.VisualStudio.2022.Community 
 # Adobe Creative Cloud
-winget install XPDLPKWG9SW2WD --accept-package-agreements
+winget install XPDLPKWG9SW2WD 
+winget install Pylogmon.pot
+winget install ShareX.ShareX
+winget install OBSProject.OBSStudio
+winget install Telegram.TelegramDesktop
+winget install IObit.AdvancedSystemCare
+winget install UderzoSoftware.SpaceSniffer
+winget install Obsidian.Obsidian
+
+winget install BleachBit.BleachBit
 
 # 翻墙安装
-winget install Notepad++.Notepad++ --accept-package-agreements
-winget install QuestSoft.QTranslate --accept-package-agreements
-winget install Initex.Proxifier --accept-package-agreements
-winget install calibre.calibre --accept-package-agreements
-winget install QL-Win.QuickLook --accept-package-agreements
-winget install ChidiWilliams.Buzz --accept-package-agreements
-winget install Datronicsoft.SpacedeskDriver.Server --accept-package-agreements
-winget install TorProject.TorBrowser --accept-package-agreements -l "C:\Programs\Tor"
-winget install RaspberryPiFoundation.RaspberryPiImager --accept-package-agreements
-winget install GnuCash.GnuCash --accept-package-agreements
+winget install Notepad++.Notepad++ 
+winget install QuestSoft.QTranslate 
+winget install Initex.Proxifier 
+winget install calibre.calibre 
+winget install QL-Win.QuickLook 
+winget install ChidiWilliams.Buzz 
+winget install Datronicsoft.SpacedeskDriver.Server 
+winget install TorProject.TorBrowser  -l "C:\Programs\Tor"
+winget install RaspberryPiFoundation.RaspberryPiImager 
+winget install GnuCash.GnuCash 
 
 # 环境
 nvm install 18.16.1
 nvm use 18.16.1
-winget install AdoptOpenJDK.OpenJDK.8 --accept-package-agreements
+nvm install latest
+winget install AdoptOpenJDK.OpenJDK.8 
 # RedisInsight
-winget install XP8K1GHCB0F1R2 --accept-package-agreements
+winget install XP8K1GHCB0F1R2 
 
 
 ####
@@ -70,13 +87,42 @@ winget install XP8K1GHCB0F1R2 --accept-package-agreements
 # https://www.oracle.com/database/sqldeveloper/technologies/download/
 # https://gpt4all.io/index.html
 # https://www.daokeyuedu.com/
+# https://github.com/josStorer/RWKV-Runner/releases/
+# https://www.nvidia.cn/geforce/drivers/
+# https://developer.nvidia.com/cuda-11-7-0-download-archive?target_os=Windows&target_arch=x86_64&target_version=11&target_type=exe_local
+# https://developer.nvidia.com/compute/cudnn/secure/8.5.0/local_installers/11.7/cudnn-windows-x86_64-8.5.0.96_cuda11-archive.zip
+
 #
 # 报错
 # https://github.com/GyanD/codexffmpeg/releases
-winget install Gyan.FFmpeg --accept-package-agreements
+winget install Gyan.FFmpeg 
 # https://www.fosshub.com/SpaceSniffer.html
-winget install UderzoSoftware.SpaceSniffer --accept-package-agreements
+winget install UderzoSoftware.SpaceSniffer 
 # https://1.1.1.1/
-winget install Cloudflare.Warp --accept-package-agreements
+winget install Cloudflare.Warp 
 # https://www.tradingview.com/desktop/
-winget install TradingView.TradingViewDesktop --accept-package-agreements
+winget install TradingView.TradingViewDesktop 
+
+
+winget install Electrum.Electrum
+# https://electrum.org/#download
+# https://www.princexml.com/download/
+# pot
+# ShareX, OBS Studio, Filmora
+
+
+
+ ARIMA 和 GARCH 模型
+
+
+
+winget install Bopsoft.Listary
+# Listary 激活码
+E-Mail: samoray@samoray.com
+Key:
+JE4V8T3M96PWT4SUCNZNVZ37XKLBU2QW
+N64LEJQ3VHY7MPL6KY2R5SQZ76QFFTKP
+VVE8JBVEAME8MMBRHRGF2P6MAJG7ZSQY
+MUY2PGTQ2EG3W2YHTU2CFWL7SE6THU3Q
+TAL3U894S2BWA9629EFCXYYYG3S65WEQ
+N3SZSNSEGXMEUWD27PNENP4GR2EKKDK6
